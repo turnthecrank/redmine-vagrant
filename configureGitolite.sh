da1085d24de74b872fd2c58bedb4bc7958336419
Philip Chase <pbc@ufl.edu>
2013-06-02 06:27:24 -0400

    Change sudo use in configureGitolite.sh so shells are no longer invoked

--- configureGitolite.sh
+++ configureGitolite.sh
@@ -8,15 +8,10 @@ sudo chmod 700 ~www-data/.ssh
 sudo chown www-data.www-data ~www-data/.ssh
 
 # as user www-data, make those keys with the default names used by Redmine Git Hosting Plugin
-sudo su - www-data
-ssh-keygen -N '' -f ~/.ssh/redmine_gitolite_admin_id_rsa
+sudo -u www-data ssh-keygen -N '' -f ~/.ssh/redmine_gitolite_admin_id_rsa
 
 # Still as user www-data, link private key to default ssh keyfile name to simplify testing
-cd ~/.ssh
-ln -s redmine_gitolite_admin_id_rsa id_rsa
-
-# we're done being user www-data
-exit
+sudo -u www-data ln -s ~www-data/.ssh/redmine_gitolite_admin_id_rsa ~www-data/.ssh/id_rsa
 
 # As user root, copy the ssh public key redmine will use to connect to the gitolite admin repo
 # make it aacessible for gitolite setup
@@ -24,19 +19,17 @@ sudo cp ~www-data/.ssh/redmine_gitolite_admin_id_rsa.pub ~git/
 sudo chown git.git ~git/redmine_gitolite_admin_id_rsa.pub
 
 # As user git, configure git.  It shouldn't be needed, but we should set reasonable defaults
-sudo su - git
-git config --global user.email "redmine@localhost"
-git config --global user.name "Redmine"
+sudo -u git git config --global user.email "redmine@localhost"
+sudo -u git git config --global user.name "Redmine"
 
 # Still as user git clean up any messes from failed gitolite setups
-rm .ssh/authorized_keys
-rm ~/.gitolite/keydir/*
-rm -rf repositories/gitolite-admin.git/
-rm gitolite-admin.pub
+sudo -u git rm ~git/.ssh/authorized_keys
+sudo -u git rm ~git/.gitolite/keydir/*
+sudo -u git rm -rf ~git/repositories/gitolite-admin.git/
+sudo -u git rm ~git/gitolite-admin.pub
 
 # Symlink our public key to the default gitolite admin, "gitolite-admin"
-ln -s redmine_gitolite_admin_id_rsa.pub gitolite-admin.pub
+sudo -u git ln -s ~git/redmine_gitolite_admin_id_rsa.pub ~git/gitolite-admin.pub
 
 # Still as user git, setup Gitolite
-gitolite setup -pk gitolite-admin.pub
-exit
+sudo -u git ~git/bin/gitolite setup -pk ~git/gitolite-admin.pub
